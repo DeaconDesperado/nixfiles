917af7f4e2d385a01b5621e191e9ed0e2649a96b
Mark Grey <mgthesecond@spotify.com>
2023-04-24 15:26:54 -0400

    initial

---
+++ configuration.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, ... }:
+
+{
+  # Nix configuration ------------------------------------------------------------------------------
+
+  nix.settings = {
+    trusted-users = [ "@admin" ];
+    substituters = [ "https://cache.nixos.org/" ];
+    trusted-public-keys =
+      [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  };
+  nix.configureBuildUsers = true;
+
+  # Enable experimental nix command and flakes
+  # nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    auto-optimise-store = true
+    experimental-features = nix-command flakes
+  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Apps
+  # `home-manager` currently has issues adding them to `~/Applications`
+  # Issue: https://github.com/nix-community/home-manager/issues/1341
+  environment.systemPackages = with pkgs; [
+    terminal-notifier
+  ];
+
+  programs.nix-index.enable = true;
+
+  # Fonts
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs; [
+    recursive
+    nerdfonts
+    iosevka-bin
+    (iosevka-bin.override { variant = "ss09"; })
+    font-awesome
+    cascadia-code
+    fira-code
+    jetbrains-mono
+  ];
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+}
---
+++ home.nix
@@ -0,0 +1,320 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.stateVersion = "22.11";
+
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+
+      withNodeJs = true;
+    };
+
+    helix = {
+      enable = true;
+      settings = {
+        theme = "rose_pine";
+        editor = {
+          line-number = "relative";
+          mouse = true;
+          lsp.display-messages = true;
+          cursorline = true;
+          indent-guides.render = true;
+          color-modes = true;
+        };
+        keys.normal = { space.space = "file_picker"; };
+        editor.cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        editor.file-picker = { hidden = false; };
+      };
+      languages = [
+        { name = "sql"; }
+        { name = "latex"; }
+        { name = "html"; }
+        { name = "bash"; }
+        { name = "toml"; }
+        { name = "nix"; }
+        { name = "markdown"; }
+        { name = "yaml"; }
+        { name = "dockerfile"; }
+        {
+          name = "json";
+          formatter = {
+            command = "prettier";
+            args = [ "--parser" "json" ];
+          };
+        }
+        { name = "go"; }
+        { name = "java"; }
+        {
+          name = "scala";
+          scope = "source.scala";
+          roots = [ "build.sbt" "pom.xml" ];
+          file-types = [ "scala" "sbt" ];
+          comment-token = "//";
+          indent = {
+            tab-width = 2;
+            unit = "  ";
+          };
+          language-server = { command = "metals"; };
+          config = { metals.ammoniteJvmProperties = [ "-Xmx1G" ]; };
+        }
+        { name = "rust"; }
+        { name = "python"; }
+        { name = "javascript"; }
+        { name = "typescript"; }
+      ];
+    };
+
+    htop.enable = true;
+
+    git = {
+      enable = true;
+      userName = "mgthesecond";
+      userEmail = "mgthesecond@spotify.com";
+
+      aliases = {
+        s = "status";
+        l =
+          "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+
+      extraConfig = {
+        color.ui = true;
+        branch.autosetuprebase = "always";
+        pull.rebase = true;
+        init.defaultBranch = "main";
+        push.default = "tracking";
+        hub.host = "ghe.spotify.net";
+      };
+
+      ignores = [ ".java_version" ".metals" "metals.sbt" ".bloop" ".idea" ".DS_Store" ];
+
+      delta.enable = true;
+    };
+
+    tmux = {
+      enable = true;
+      terminal = "xterm-256color";
+      baseIndex = 1;
+      keyMode = "vi";
+      shortcut = "s";
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        yank
+        {
+          plugin = dracula;
+          extraConfig = ''
+            set -g @dracula-plugins "cpu-usage ram-usage"
+            set -g @dracula-show-battery false
+            set -g @dracula-show-powerline true
+            set -g @dracula-refresh-rate 10
+            # set -g @dracula-show-left-icon window
+          '';
+        }
+      ];
+
+      extraConfig = ''
+        set -ga terminal-overrides ",xterm-256color*:Tc"
+        set -g mouse on
+
+        # act like vim
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+        bind-key -r C-h select-window -t :-
+        bind-key -r C-l select-window -t :+
+
+        # set -g prefix2 C-s
+        # renumber windows sequentially after closing any of them
+        set -g renumber-windows on
+
+        # prefix -> back-one-character
+        bind-key C-b send-prefix
+        # prefix-2 -> forward-incremental-history-search
+        bind-key C-s send-prefix -2
+
+        # don't suspend-client
+        unbind-key C-z
+      '';
+    };
+
+    zsh = {
+      enable = true;
+      autocd = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
+      history.extended = true;
+      shellAliases = {
+        cat = "bat";
+        git = "hub";
+        k = "kubectl";
+      };
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./config/p10k;
+          file = "p10k.zsh";
+        }
+      ];
+      initExtra = ''
+        export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+        export LSP_USE_PLISTS=true
+        export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=cyan,bg=bold,underline"
+        export PATH=$PATH:/opt/homebrew/bin:$HOME/go/bin
+      '';
+    };
+
+    broot.enable = true;
+
+    bat = {
+      enable = true;
+      config.theme = "ansi";
+    };
+
+    fzf.enable = true;
+
+    alacritty.enable = true;
+
+    vscode = {
+      enable = true;
+    };
+
+    java = {
+      enable = true;
+      package = pkgs.jdk;
+    };
+
+    direnv.enable = true;
+    direnv.nix-direnv.enable = true;
+
+    texlive = {
+      enable = true;
+      extraPackages = tpkgs: { inherit (tpkgs) scheme-full; };
+    };
+
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
+
+    zoxide.enable = true;
+  };
+
+  home.packages = with pkgs;
+    [
+      fx
+      krew
+      marksman
+      emacs
+      httpie
+      obsidian
+      d2
+      cloud-sql-proxy
+      k9s
+      pluto
+      nodePackages.dockerfile-language-server-nodejs
+      ltex-ls
+      languagetool
+      rnix-lsp
+      jdt-language-server
+      texlab
+      editorconfig-checker
+      hugo
+      colima
+      act
+      avro-tools
+      bazel_5
+      bazel-buildtools
+      coreutils
+      curl
+      delta
+      deno
+      ditaa
+      duf
+      html-tidy
+      gdb
+      gettext
+      gh
+      gitAndTools.hub
+      gnupg
+      gnused
+      go
+      (google-cloud-sdk.withExtraComponents
+        ([ google-cloud-sdk.components.gke-gcloud-auth-plugin ]))
+      gopls
+      jdk
+      gradle
+      graphviz-nox
+      imagemagick
+      jq
+      kind
+      kubebuilder
+      kubectx
+      kubernetes
+      kubernetes-helm
+      kustomize
+      leiningen
+      maven
+      metals
+      minikube
+      mosh
+      nixfmt
+      nixpkgs-fmt
+      nodePackages.generator-code
+      nodePackages.mermaid-cli
+      nodePackages.prettier
+      nodePackages.sql-formatter
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.yaml-language-server
+      nodePackages.vscode-json-languageserver
+      nodePackages.yarn
+      nodePackages.yo
+      nodePackages.pyright
+      nodePackages_latest.bash-language-server
+      nodePackages_latest.markdownlint-cli
+      nodejs-16_x
+      operator-sdk
+      pandoc
+      parallel
+      protobuf
+      (python3.withPackages
+        (ps: with ps; with python3Packages; [
+          pip
+          readline
+          sqlparse
+          python-lsp-server
+        ]))
+      ripgrep
+      rustup
+      sbt
+      scala
+      scala-cli
+      scalafmt
+      shellcheck
+      silver-searcher
+      tldr
+      trino-cli
+      watch
+      wget
+      yarn2nix
+      yq-go
+    ] ++ lib.optionals stdenv.isDarwin [
+      cocoapods
+      m-cli # useful macOS CLI commands
+    ];
+
+}
