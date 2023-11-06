8e241f0ab69c5786aaa451deab880d5abf24830f
Mark Grey <mgthesecond@spotify.com>
2023-11-06 14:13:23 -0500

    pyright, unify lsp settings

--- config/lsp/jdtls.lua
+++ config/lsp/jdtls.lua
@@ -26,7 +26,7 @@ local on_attach = function(client, bufnr)
   nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
   nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
   nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
-  nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
+  nnoremap('<C-space>', vim.lsp.buf.hover, bufopts, "Hover text")
   nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
   nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
   nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
@@ -35,13 +35,13 @@ local on_attach = function(client, bufnr)
   end, bufopts, "List workspace folders")
   nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
   nnoremap('<space>rn', vim.lsp.buf.rename, bufopts, "Rename")
-  nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
-  vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+  nnoremap('<Leader>a', vim.lsp.buf.code_action, bufopts, "Code actions")
+  vim.keymap.set('v', "<Leader>a", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
     { noremap=true, silent=true, buffer=bufnr, desc = "Code actions" })
   nnoremap('<space>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
 
   -- Java extensions provided by jdtls
-  nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+  nnoremap("<C-i>", jdtls.organize_imports, bufopts, "Organize imports")
   nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
   nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
   vim.keymap.set('v', "<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
