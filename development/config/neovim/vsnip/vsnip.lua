f4121a3775aeffb5ad3f2f9815bbc2c5750bcfbe
Mark Grey <mgthesecond@spotify.com>
2024-10-26 21:13:45 -0400

    improved tab for snippets

--- development/config/neovim/vsnip/vsnip.lua
+++ development/config/neovim/vsnip/vsnip.lua
@@ -1,10 +1,36 @@
-local function jump(direction, alias, key)
-  if vim.fn["vsnip#jumpable"](direction) then
-   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>(" .. alias .. ")", true, true, true))
-  else
-    vim.fn.feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true))
-  end
+local t = function(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-vim.keymap.set({'i', 'v'}, '<Tab>', function () jump(1, "vsnip-jump-next", "<Tab>") end)
-vim.keymap.set({'i', 'v'}, '<S-Tab>', function () jump(1, "vsnip-jump-prev", "<S-Tab>") end)
+local check_back_space = function()
+	local col = vim.fn.col('.') - 1
+	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+_G.tab_complete = function()
+	if vim.fn.pumvisible() == 1 then
+		return t('<C-n>')
+	elseif vim.fn['vsnip#available'](1) == 1 then
+                -- edit here --
+		vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '')
+                 return ''
+	else
+		return t('<Tab>')
+	end
+end
+
+_G.s_tab_complete = function()
+	if vim.fn.pumvisible() == 1 then
+		return t('<C-p>')
+	elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+               -- edit here --
+		vim.fn.feedkeys(t('<Plug>(vsnip-jump-prev)'), '')
+                 return ''
+	else
+		-- If <S-Tab> is not working in your terminal, change it to <C-h>
+		return t('<S-Tab>')
+	end
+end
+
+vim.keymap.set({'i', 'v'}, '<Tab>', _G.tab_complete, {expr = true})
+vim.keymap.set({'i', 'v'}, '<S-Tab>', _G.s_tab_complete, {expr = true})
