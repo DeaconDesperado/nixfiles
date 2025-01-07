0453539239f7b2392bc9cf778945b181e89e9937
Mark Grey <mgthesecond@spotify.com>
2025-01-07 11:26:05 -0500

    rebind conflicting keybind, panel auto-refresh

--- development/config/neovim/copilot/copilot-chat.lua
+++ development/config/neovim/copilot/copilot-chat.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_user_command('LoadCopilot', function(input)
   vim.cmd.packadd({args = {"copilot.lua"} });
   vim.cmd.packadd({args = {"CopilotChat.nvim"} });
-  require('copilot').setup({});
+  require('copilot').setup({
+    panel = {
+      auto_refresh = true;
+    }
+  });
   require('CopilotChat').setup({});
 end, {})
